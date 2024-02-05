be9d391e5afd6942f5d36975e2f5847b9b6d75af
Pratik Bakshi <bakshi.pratikcool@gmail.com>
2024-02-05 13:36:01 +0530

    Api added and list render successfully

--- src/App.js
+++ src/App.js
@@ -1,60 +1,49 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 
+const base_url_api = 'https://fake-store-api.mock.beeceptor.com/api/products';
+
 const App = () => {
+
+    const [productData, setProductData] = useState('');
+
+    useEffect(() => {
+        getAPIData();
+        // console.log(productData);
+    }, [])
+
+    const getAPIData = async () => {
+        const response = await fetch(base_url_api);
+
+        const data = await response.json();
+        console.log(data);
+
+        setProductData(data);
+    }
+
+
     return (
         <div className='App'>
             <section>
                 <h1>Product Listing Page</h1>
                 <div className="card-container">
-                  
-                <div className="card">
-                    <img src='' alt="productImage" />
-                    <h2>Product Title</h2>
-                    <p>price</p>
-                    <p>stock status</p>
-                    <button>Add to cart</button>
-                </div>
+                    {
+                        productData &&
+                        productData.map((product, index) => {
 
-                <div className="card">
-                    <img src='' alt="productImage" />
-                    <h2>Product Title</h2>
-                    <p>price</p>
-                    <p>stock status</p>
-                    <button>Add to cart</button>
-                </div>
+                            const { image, brand, price, availability } = product;
 
-                <div className="card">
-                    <img src='' alt="productImage" />
-                    <h2>Product Title</h2>
-                    <p>price</p>
-                    <p>stock status</p>
-                    <button>Add to cart</button>
-                </div>
-
-                <div className="card">
-                    <img src='' alt="productImage" />
-                    <h2>Product Title</h2>
-                    <p>price</p>
-                    <p>stock status</p>
-                    <button>Add to cart</button>
-                </div>
-
-                <div className="card">
-                    <img src='' alt="productImage" />
-                    <h2>Product Title</h2>
-                    <p>price</p>
-                    <p>stock status</p>
-                    <button>Add to cart</button>
-                </div>
-
-                <div className="card">
-                    <img src='' alt="productImage" />
-                    <h2>Product Title</h2>
-                    <p>price</p>
-                    <p>stock status</p>
-                    <button>Add to cart</button>
-                </div>
+                            return (
+                                <div className="card" key={index}>
+                                    <img src={image} alt="image" />
+                                    <h2>{brand}</h2>
+                                    <p>{price}</p>
+                                    <p>{availability ? "in-Stock" : "out-of-Stock"}</p>
+                                    <button>Add to cart</button>
+                                </div>
+                            )
+                        })
+                    }
                 </div>
             </section>
         </div>
