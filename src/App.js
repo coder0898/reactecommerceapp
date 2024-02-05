695036f5881b8f9b2107e0aec60723fc4d11852a
Pratik Bakshi <bakshi.pratikcool@gmail.com>
2024-02-05 11:53:11 +0530

    Product List page layout done in React

---
+++ src/App.js
@@ -0,0 +1,64 @@
+import React from 'react';
+import './App.css';
+
+const App = () => {
+    return (
+        <div className='App'>
+            <section>
+                <h1>Product Listing Page</h1>
+                <div className="card-container">
+                  
+                <div className="card">
+                    <img src='' alt="productImage" />
+                    <h2>Product Title</h2>
+                    <p>price</p>
+                    <p>stock status</p>
+                    <button>Add to cart</button>
+                </div>
+
+                <div className="card">
+                    <img src='' alt="productImage" />
+                    <h2>Product Title</h2>
+                    <p>price</p>
+                    <p>stock status</p>
+                    <button>Add to cart</button>
+                </div>
+
+                <div className="card">
+                    <img src='' alt="productImage" />
+                    <h2>Product Title</h2>
+                    <p>price</p>
+                    <p>stock status</p>
+                    <button>Add to cart</button>
+                </div>
+
+                <div className="card">
+                    <img src='' alt="productImage" />
+                    <h2>Product Title</h2>
+                    <p>price</p>
+                    <p>stock status</p>
+                    <button>Add to cart</button>
+                </div>
+
+                <div className="card">
+                    <img src='' alt="productImage" />
+                    <h2>Product Title</h2>
+                    <p>price</p>
+                    <p>stock status</p>
+                    <button>Add to cart</button>
+                </div>
+
+                <div className="card">
+                    <img src='' alt="productImage" />
+                    <h2>Product Title</h2>
+                    <p>price</p>
+                    <p>stock status</p>
+                    <button>Add to cart</button>
+                </div>
+                </div>
+            </section>
+        </div>
+    )
+}
+
+export default App;
