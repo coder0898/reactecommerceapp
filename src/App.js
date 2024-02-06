52dfbd42e852e8fe11f8a88f6742c35737a0a273
Pratik Bakshi <bakshi.pratikcool@gmail.com>
2024-02-06 13:27:12 +0530

    added search filter functionality and its working

--- src/App.js
+++ src/App.js
@@ -6,7 +6,8 @@ const base_url_api = 'https://fake-store-api.mock.beeceptor.com/api/products';
 
 const App = () => {
 
-    const [productData, setProductData] = useState('');
+    const [productData, setProductData] = useState([]);
+    const [queryInput, setQueryInput] = useState('');
 
     useEffect(() => {
         getAPIData();
@@ -16,25 +17,29 @@ const App = () => {
         const response = await fetch(base_url_api);
 
         const data = await response.json();
-        // console.log(data);
 
         setProductData(data);
     }
 
+
+    // search filter code
+    const filterProductData = productData.filter(items=>{
+        return items.brand.toLowerCase().includes(queryInput.toLowerCase());
+    })
 
     return (
         <div className='App'>
             <main>
                 <h1>Product Listing Page</h1>
                 <div className="filter-bar">
-                    <input type="search" id="searchFilter" placeholder='search a product'/>
+                    <input type="search" value={queryInput} onChange={(e)=> setQueryInput(e.target.value)} placeholder='search a product'/>
                 </div>
                 <div className="card-container">
                     {
                         productData &&
-                        productData.map((product, i) => {
+                        filterProductData.map((products, i) => {
                             return (
-                               <Card product={product} key={i}/>
+                               <Card product={products} key={i}/>
                             )
                         })
                     }
